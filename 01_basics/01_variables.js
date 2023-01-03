952ac83c03d8cef22aaa088898c2220ff648f815
coder-nitin <62199700+coder-nitin@users.noreply.github.com>
2023-01-03 12:14:29 +0530

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,8 @@
-const accountId = 144553
-let accountEmail = "nitin@google.com"
-var accountPassword = "12345"
-accountCity = "Indore"
-let accountState
+const accountId = 144553;
+let accountEmail = "nitin@google.com";
+var accountPassword = "12345";
+accountCity = "Indore";
+let accountState;
 
 /*
 prefer not to use var
@@ -10,13 +10,12 @@ because of issue in block scope and functional scope
 */
 
 // accountId = 2 ->not allowed will give error
-accountEmail = "nc@nc.com"
-accountPassword = "54321"
-accountCity = "Sendhwa"
+accountEmail = "nc@nc.com";
+accountPassword = "54321";
+accountCity = "Sendhwa";
 
-console.log(accountId)
+console.log(accountId);
 
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
 
-//testing
 //we can use semi colons in javascript its based on choice
