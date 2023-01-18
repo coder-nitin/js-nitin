2186cb2161d95c4641e6279662f1fe60744439df
coder-nitin <62199700+coder-nitin@users.noreply.github.com>
2023-01-18 18:30:20 +0530

    js operations

---
+++ 01_basics/03.1_operations.js
@@ -0,0 +1,35 @@
+/********************OPERATIONS********************************/
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello";
+let str2 = " Nitin";
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(3+4*5%3); 
+// note:- do not rely on this type of convesrsions always use paranthesis to clarify an operational statement
+
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3 ;
+num1 = num2 = num3 = 2+2 ;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
